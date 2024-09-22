e6fa80ed3cdabe84911b353cfa6ceeaa3ecb17a0
Yannick <y.kogitos@gmail.com>
2024-09-22 13:49:15 +0200

    add eslint (TODO: end 11 + 13

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Suspense } from 'react';
 
 export default function RootLayout({
   children,
